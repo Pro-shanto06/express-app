5b8fc60d4ac0658b9202e460be337d7c3d13b80f
Proshanto Saha <proshantosaha1999@gmail.com>
2024-08-14 14:47:40 +0600

    Fixed codecy errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,42 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'airbnb-typescript/base',
-      'plugin:import/recommended',
-      'plugin:import/typescript'
-    ],
-    parserOptions: {
-      tsconfigRootDir: __dirname,
-      project: './tsconfig.json',
-    },
-    plugins: ['@typescript-eslint', 'import'],
-    rules: {
-      'import/extensions': ['error', 'ignorePackages', {
-        'ts': 'never',
-        'tsx': 'never',
-      }],
-      'import/no-extraneous-dependencies': ['error', {
-        devDependencies: true,
-        optionalDependencies: false,
-        peerDependencies: false,
-      }],
-    },
-  };
-  
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript/base',
+    'plugin:import/recommended',
+    'plugin:import/typescript'
+  ],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+    sourceType: 'module'  
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  rules: {
+    'import/extensions': ['error', 'ignorePackages', {
+      'ts': 'never',
+      'tsx': 'never',
+      'js': 'never',
+      'jsx': 'never'
+    }],
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: true,
+      optionalDependencies: false,
+      peerDependencies: false,
+    }],
+    'import/no-unresolved': ['error', {
+      commonjs: true, 
+      amd: true,
+      caseSensitive: true, 
+    }],
+    'import/resolver': {
+      'node': {
+        'extensions': ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      'typescript': {
+        'alwaysTryTypes': true,
+      }
+    }
+  },
+};
