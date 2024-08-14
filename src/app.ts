ca20a238332a57f1a2e5e5fa8041ec204f4fa490
Proshanto Saha <proshantosaha1999@gmail.com>
2024-08-14 11:46:50 +0600

    Implemented Rate Limit

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,17 @@ import express, { Request, Response } from 'express';
 import corsMiddleware from './middleware/corsMiddleware';
 import helloWorldRoutes from './routes/helloWorldRoutes';
 import axios from 'axios';
+import rateLimiter from './middleware/rateLimitMiddleware';
 
 const app = express();
 app.use(corsMiddleware);
+app.use(rateLimiter);
 
 // app.get('/', (req: Request, res: Response) => {
 //   res.send('Hello! This is a Express App.');
 // });
 
-app.get('/', async (req, res) => {
+app.get('/', async (req: Request, res: Response) => {
   try {
     const response = await axios.get('http://localhost:4000');
     res.send(response.data);
