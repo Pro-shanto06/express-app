e96420fab2bda5bff92237c0577f40de4dc3d4e9
Proshanto Saha <proshantosaha1999@gmail.com>
2024-08-14 14:38:02 +0600

    Fixed codecy errors

--- src/app.ts
+++ src/app.ts
@@ -8,18 +8,21 @@ const app = express();
 app.use(corsMiddleware);
 app.use(rateLimiter);
 
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Hello! This is a Express App.');
-// });
-
-app.get('/', async (req: Request, res: Response) => {
-  try {
-    const response = await axios.get('http://localhost:4000');
-    res.send(response.data);
-  } catch (error) {
-    res.status(500).send('Error occurred while fetching data.');
-  }
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello! This is a Express App.');
 });
+
+
+// For checking cors middleware
+
+// app.get('/', async (req: Request, res: Response) => {
+//   try {
+//     const response = await axios.get('http://localhost:4000');
+//     res.send(response.data);
+//   } catch (error) {
+//     res.status(500).send('Error occurred while fetching data.');
+//   }
+// });
 
 app.use('/api', helloWorldRoutes);
 
